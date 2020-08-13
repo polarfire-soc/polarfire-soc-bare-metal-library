0a5ca0baafea77e166550479a1dbf43845b89049
Conor <conor.paxton@microchip.com>
2020-08-13 15:09:53 +0000

    Squashed 'src/platform/drivers/mss_can/' changes from dc260e4..87c266a
    87c266a Changes Included: 1. Remove extra file included. 2. Updated Hardware Flow Dependencies section
    ea336cc Replaced multiple hal  file inclusion by #include "mss_hal.h" file
    
    git-subtree-dir: src/platform/drivers/mss_can
    git-subtree-split: 87c266acc31cdec71adc2cac89b04bab0c4c3170

--- mss_can.c
+++ mss_can.c
@@ -9,14 +9,10 @@
  */
 
 /*******************************************************************************
- * include files
+ * Include files
  */
-#include <stddef.h>
-#include <stdint.h>
+#include "mpfs_hal/mss_hal.h"
 #include "mss_can.h"
-#include "mpfs_hal/mss_sysreg.h"
-#include "mpfs_hal/mss_plic.h"
-#include "mpfs_hal/mss_util.h"
 
 #ifdef __cplusplus
 extern "C" {
